2bb8f9e1ef46144fa672268c71eddd43a050cf14
christian3-tech <tianpendo3@gmail.com>
2025-12-11 13:56:07 +0800

    Add files via upload

---
+++ TipeDataDemo.java
@@ -0,0 +1,12 @@
+public class TipeDataDemo {
+    public static void main(String[] args) {
+
+        int umur = 20;              // tipe data integer
+        double tinggi = 165.5;      // tipe data pecahan
+        String nama = "Andi";       // tipe data teks
+
+        System.out.println("Nama   : " + nama);
+        System.out.println("Umur   : " + umur);
+        System.out.println("Tinggi : " + tinggi);
+    }
+}
